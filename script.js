19935221ebeef7fd5ca9b7406c5f851cf978ea58
itsmikeysmith <152660570+itsmikeysmith@users.noreply.github.com>
2024-04-14 17:27:26 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//* Get the button
+let _about = document.getElementById("goHome");
+
+//* When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
