57a116247e601babce4bea0427defba422a049a7
itsmikeysmith <152660570+itsmikeysmith@users.noreply.github.com>
2024-04-14 17:25:31 -0600

    Delete final directory

--- final/script.js
+++
@@ -1,8 +0,0 @@
-//* Get the button
-let _about = document.getElementById("goHome");
-
-//* When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
